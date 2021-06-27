5e0e07fb2882f2cce5a4652086afa0d6c17bc8d8
Kamesh Kotwani <36355951+kameshkotwani@users.noreply.github.com>
2021-06-27 09:13:23 +0530

    Create 25.redundant-connection.cpp

---
+++ 25.redundant-connection.cpp
@@ -0,0 +1,42 @@
+const int N = 1000;
+class Solution {
+public:
+   int parent[N + 5];
+   int rank[N + 5];
+   int getParent(int n){
+      if (parent[n] == -1)
+         return n;
+      return parent[n] = getParent(parent[n]);
+   }
+   bool unionn(int a, int b){
+      int pa = getParent(a);
+      int pb = getParent(b);
+      if (pa == pb)
+         return false;
+      if (rank[pa] > rank[pb]) {
+         rank[pa] += rank[pb];
+         parent[pb] = pa;
+      }
+      else {
+         rank[pb] += rank[pa];
+         parent[pa] = pb;
+      }
+      return true;
+   }
+   vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+      int n = edges.size();
+      for (int i = 0; i < n; i++) {
+         parent[edges[i][0]] = parent[edges[i][1]] = -1;
+         rank[edges[i][0]] = rank[edges[i][1]] = 1;
+      }
+      vector<int> ans;
+      for (int i = 0; i < n; i++) {
+         int u = edges[i][0];
+         int v = edges[i][1];
+         if (!unionn(u, v)) {
+            ans = edges[i];
+         }
+      }
+      return ans;
+   }
+};
